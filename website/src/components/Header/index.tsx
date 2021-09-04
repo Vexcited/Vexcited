5d442393c23874eeadb368215ad19108dabf0a31
Mikkel RINGAUD <vexitofficial@gmail.com>
2021-09-04 11:14:31 +0200

    Fixed Sass errors + Modified colors

--- website/src/components/Header/index.tsx
+++ website/src/components/Header/index.tsx
@@ -1,11 +1,12 @@
 import styles from "./styles.module.scss";
-import Logo from "../../assets/logo.svg";
-import React from "react";
+import Logo from "assets/logo.svg";
 
 export default function Header(): JSX.Element {
 	return (
 		<header>
-			<img className={styles.imageLogo} src={Logo} alt="Vexcited" />
+			<a className={styles.imageLogoLink} href="/">
+				<img className={styles.imageLogo} src={Logo} alt="Vexcited" />
+			</a>
 		</header>
 	);
 }
--- website/src/index.tsx
+++ website/src/index.tsx
@@ -2,18 +2,24 @@ import ReactDOM from "react-dom";
 import React from "react";
 
 // Components
-import Header from "@components/Header";
+import Header from "components/Header";
 
 // Global CSS
-import "@assets/globals.css";
+import "assets/globals.scss";
 
 ReactDOM.render(
 	<React.StrictMode>
 		<Header />
-		<p>Le site internet est en cours de reconstruction ! Merci de patienter un peu avant sa réouverture.</p>
-		<p>En attendant vous pouvez faire un tour sur mon
-			<a href="https://github.com/Vexcited" target="_blank" rel="noreferrer">GitHub</a>
-			et sur mon <a href="https://instagram.com/vexcitedoff" target="_blank" rel="noreferrer">Instagram</a> !
+		<p>Le site est en cours de reconstruction ! Merci de patienter un peu avant sa réouverture.</p>
+		<p>En attendant vous pouvez faire un tour sur mon {" "}
+			<a href="https://github.com/Vexcited" target="_blank" rel="noreferrer">
+				GitHub
+			</a>
+			{" "} et sur mon {" "}
+			<a href="https://instagram.com/vexcitedoff" target="_blank" rel="noreferrer">
+				Instagram
+			</a>
+			{" "} !
 		</p>
 	</React.StrictMode>,
 	document.getElementById("root")
