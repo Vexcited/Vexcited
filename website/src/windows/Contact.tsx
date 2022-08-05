73d89a0cf0602ec6f1316fee30d0491e0f29d274
Mikkel RINGAUD <mikkel@milescode.dev>
2022-08-05 17:36:10 +0200

    feat: create `Contact` component

---
+++ website/src/windows/Contact.tsx
@@ -0,0 +1,14 @@
+import type { Component } from "solid-js";
+
+const ContactWindow: Component = () => {
+
+  return (
+    <div class="p-4">
+      <h1>Contact</h1>
+
+      <p>This section is still in development. Please come back later !</p>
+    </div>
+  );
+};
+
+export default ContactWindow;
