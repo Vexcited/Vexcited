d5a573f21b59ce76eb6233bbdbfdfe36f4964580
Mikkel ALMONTE--RINGAUD <contact@vexcited.com>
2026-01-15 13:27:44 +0100

    feat: add projects and intro section (at least to get content)

--- website/src/main.tsx
+++ website/src/main.tsx
@@ -1,5 +1,8 @@
 /* @refresh reload */
-import "@fontsource/poppins";
+import "@fontsource/poppins/300.css";
+import "@fontsource/poppins/400.css";
+import "@fontsource/poppins/500.css";
+import "@fontsource/poppins/600.css";
 import "@fontsource/poppins/900-italic.css";
 import "@fontsource/poppins/900.css";
 import "@fontsource/fira-code";
